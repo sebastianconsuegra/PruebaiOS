6692f9f7d916834b42b321870db6ed81481b91fa
GIRSI <administrador@092628-docent-o.local>
2017-09-27 16:03:29 -0500

    Probando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+	public data;
+	public n;
 
   constructor(public navCtrl: NavController) {
-
+  	this.data=["hola0","hola1","hola2"]
+  	
+  	this.n=0;
   }
+
+
+
+play(){
+
+	// var x: string[];
+ //  	x=["hola","hola2","hola3"]
+	//console.log(x[1])
+	this.n=this.n+1;
+
+}
+
+
 
 }
